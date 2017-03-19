6d5941a57e500fc0b83ac4e5c0970f12396a5687
Sintax <sintax@obscurepacket.org>
2017-03-19 09:09:32 -0500

    Added setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='scadasim',
+      version='0.1',
+      description='SCADA Simulator encompassing things from PLCs to devices such as valves, pumps, tanks, etc. and environmental properties such as water pH levels',
+      url='https://github.com/sintax1/scadasim',
+      author='sintax1',
+      author_email='sintax@obscurepacket.org',
+      license='MIT',
+      packages=['scadasim'],
+      zip_safe=False)
+
