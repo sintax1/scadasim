a178d514b19b34779d112c72ef610941810253b5
Sintax <sintax@obscurepacket.org>
2017-03-23 11:17:19 -0500

    updated dbus handlers

--- scadasim/dbusservice/dbusservice.py
+++ scadasim/dbusservice/dbusservice.py
@@ -48,6 +48,7 @@ class DBusService(threading.Thread):
 
         for plc in self.plcs:
             for sensor in self.plcs[plc]['sensors']:
+                log.debug(self.plcs[plc]['sensors'][sensor])
                 read_sensor = self.plcs[plc]['sensors'][sensor]['read_sensor']
                 self.plcs[plc]['sensors'][sensor]['value'] = read_sensor()
 
