e9834a9bd28705c45f908663264e398a729a056a
Sintax <sintax@obscurepacket.org>
2017-03-22 21:32:22 -0500

    Added dbus handlers for registerplc and readsensors

--- scadasim/dbusservice/dbusservice.py
+++ scadasim/dbusservice/dbusservice.py
@@ -23,13 +23,30 @@ class DBusWorker(dbus.service.Object):
 
         dbus.service.Object.__init__(self, self.name, '/')
 
+    """
+    Coil/Register Numbers   Data Addresses  Type        Table Name
+    1-9999                  0000 to 270E    Read-Write  Discrete Output Coils
+    10001-19999             0000 to 270E    Read-Only   Discrete Input Contacts
+    30001-39999             0000 to 270E    Read-Only   Analog Input Registers
+    40001-49999             0000 to 270E    Read-Write  Analog Output Holding Registers
+
+    Each coil or contact is 1 bit and assigned a data address between 0000 and 270E.
+    Each register is 1 word = 16 bits = 2 bytes
+
+    """
+
+    #https://dbus.freedesktop.org/doc/dbus-python/doc/tutorial.html#basic-type-conversions
     @dbus.service.method("com.root9b.scadasim", in_signature='s', out_signature='as')
-    def HelloWorld(self, hello_message):
-        return ["Hello", "from example-service.py", "with unique name", self.session_bus.get_unique_name()]
+    def registerPLC(self, hostname):
+        """
+            return sensor name and sensor address in PLC.
+                TODO: add slave id
+        """
+        return {'Sensor1': 0x1001, 'Sensor2': 0x2001}
 
     @dbus.service.method("com.root9b.scadasim", in_signature='', out_signature='')
-    def Exit(self):
-        self.loop.quit()
+    def readSensors(self, hostname):
+        return {'Sensor1': 0x00, 'Sensor2': 0xff}
 
 
 if __name__ == '__main__':
