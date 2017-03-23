d4bb49c01c8c1f8fee63c3b6678245c0a84d6ca6
Sintax <sintax@obscurepacket.org>
2017-03-23 09:19:31 -0500

    fixed typo

--- scadasim/simulator.py
+++ scadasim/simulator.py
@@ -9,63 +9,63 @@ log.setLevel(logging.DEBUG)
 
 class Simulator(object):
 
-	def __init__(self, dbus=False):
-		self.dbus = dbus
-		if self.dbus:
-			from dbusservice import DBusService
-        	self.dbusservice = DBusService()
+    def __init__(self, dbus=False):
+        self.dbus = dbus
+        if self.dbus:
+            from dbusservice import DBusService
+            self.dbusservice = DBusService()
 
-	def load_yml(self, path_to_yaml_config):
-		"""Read and parse YAML configuration file into simulator devices
-		"""
-		self.path_to_yaml_config = path_to_yaml_config
-		self.config = parse_yml(path_to_yaml_config)
-		simulation = build_simulation(self.config)
-		self.settings = simulation['settings']
-		self.devices = simulation['devices']
-		self.sensors = simulation['sensors']
-		self.plcs = simulation['plcs']
+    def load_yml(self, path_to_yaml_config):
+        """Read and parse YAML configuration file into simulator devices
+        """
+        self.path_to_yaml_config = path_to_yaml_config
+        self.config = parse_yml(path_to_yaml_config)
+        simulation = build_simulation(self.config)
+        self.settings = simulation['settings']
+        self.devices = simulation['devices']
+        self.sensors = simulation['sensors']
+        self.plcs = simulation['plcs']
 
-		self.set_speed(self.settings['speed'])
+        self.set_speed(self.settings['speed'])
 
-	def start(self):
-		"""Start the simulation"""
-		for device in self.devices.values():
-			device.activate()
+    def start(self):
+        """Start the simulation"""
+        for device in self.devices.values():
+            device.activate()
 
-		for sensor in self.sensors.values():
-			sensor.activate()
+        for sensor in self.sensors.values():
+            sensor.activate()
 
-		if self.dbus:
-        	self.dbusservice.load_plcs(self.plcs)
-        	self.dbusservice.set_speed(self.settings['speed'])
-        	self.dbusservice.start()
+        if self.dbus:
+            self.dbusservice.load_plcs(self.plcs)
+            self.dbusservice.set_speed(self.settings['speed'])
+            self.dbusservice.start()
 
-	def pause(self):
-		"""Pause the simulation"""
-		for device in self.devices.values():
-			device.deactivate()
+    def pause(self):
+        """Pause the simulation"""
+        for device in self.devices.values():
+            device.deactivate()
 
-		for sensor in self.sensors.values():
-			sensor.deactivate()
+        for sensor in self.sensors.values():
+            sensor.deactivate()
 
-	def stop(self):
-		"""Stop and destroy the simulation"""
-		self.pause()
-		sys.exit(0)
+    def stop(self):
+        """Stop and destroy the simulation"""
+        self.pause()
+        sys.exit(0)
 
-	def set_speed(self, speed):
-		"""Increase/Decrease the speed of the simulation
-			default: 1/second
-		"""
-		for device in self.devices.values():
-			device.speed = speed
+    def set_speed(self, speed):
+        """Increase/Decrease the speed of the simulation
+            default: 1/second
+        """
+        for device in self.devices.values():
+            device.speed = speed
 
-		for sensor in self.sensors.values():
-			sensor.speed = speed
+        for sensor in self.sensors.values():
+            sensor.speed = speed
 
-	def restart(self):
-		"""Stop and reload the simulation from the original config"""
-		self.pause()
-		self.load_yml(self.path_to_yaml_config)
-		self.start()
+    def restart(self):
+        """Stop and reload the simulation from the original config"""
+        self.pause()
+        self.load_yml(self.path_to_yaml_config)
+        self.start()
